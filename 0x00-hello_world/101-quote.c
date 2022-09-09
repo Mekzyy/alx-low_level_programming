15543dffb6fb750a1b3dfdf5fcfcf40fa67ef04a
Mekzyy <emeka.pesos@icloud.com>
2022-09-08 18:52:15 -0700

    done with task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <stdio.h>   
-#include <unistd.h>   
+#include <unistd.h>
+
 /**                                                                           
  *  * main - entry point                                                         
  *  *                                                                            
  *  * Description: prints and that piece..                                       
  *  * Return: 1 if sucessful                                                      
  */                                                                           
-int main(void)                                                                
+int main(void)
 {                                                                             
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);                                                                      
 	return (1);                                                           
