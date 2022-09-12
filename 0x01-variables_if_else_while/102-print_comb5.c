1b9dff902c740fa358fab4fa3519e2ee59ccfef5
Mekzyy <emeka.pesos@icloud.com>
2022-09-12 14:40:34 -0700

    done with task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,7 +3,8 @@
 /**
  * main - print possible combination of two 2-digit
  *
- * numbers
+ * Description : print
+ *
  *
  * Return: returns zero at the end
 */
@@ -12,45 +13,32 @@ int main(void)
 
 {
 
-	int i, p;
+	int i = 0, p;
 
-	for (i = 0; i < 100; i++)
-
+	while (i <= 99)
 	{
-
-		for (p = 0; p < 100; i++)
-
+		n2 = n1;
+		while ( p <= 99)
 		{
-			if (p > i)
-
-
+			if (p != i)
 			{
-
-				putchar((i / 10) + '0');
-
-				putchar((i % 10) + '0');
-
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
 				putchar(' ');
+				putchar((p / 10) + 48);
+				putchar((p % 10) + 48);
 
-				putchar((p / 10) + '0');
-
-				putchar((p % 10) + '0');
-
-				if (i != 98)
-
+				if (i != 98 || p != 98)
 				{
-
 					putchar(',');
-
 					putchar(' ');
 				}
-
 			}
-
+			++p;
 		}
-
+		++i;
 	}
-
-	putchar('\n');
+	putchar ('\n');
 	return (0);
+
 }
