52bab3e13da5f94280310e4d07033d8441988da2
Mekzyy <emeka.pesos@icloud.com>
2022-09-12 05:52:59 -0700

    done with task

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *   * main - Prints the numbers since 0 to 9
- *   *
- *   * Return: Always (Success)
- *   */
+ * main - Prints the numbers since 0 to 9
+ *
+ * Return: Always (Success)
+ */
 int main(void)
 {
 	char n;
@@ -13,7 +13,7 @@ int main(void)
 	{
 		putchar(n);
 	}
-	
+
 	putchar('\n');
 
 	return (0);
