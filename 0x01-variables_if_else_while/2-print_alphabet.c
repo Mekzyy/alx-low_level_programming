22020b7400f5d4d061f61f9f5a96def480139d19
Mekzyy <emeka.pesos@icloud.com>
2022-09-12 04:11:59 -0700

    done with task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>                                                                                                                             
+#include <time.h>                                                                                                                               
+#include <stdio.h>                                                                                                                              
+/**
+ * main - lower case alphabets                                                                                                       
+ *                                                                                                                                              
+ * Description: using the main function                                                                                                         
+ * Return: 0                                                                                                                                    
+ */                                                                                                                                               
+                                                                                                                                                
+int main(void)                                                                                                                                  
+{     
+	char c;
+
+	for (c ='a'; c <='z'; c++)
+		putchar(c);
+
+	putchar('\n');	
+	return (0);                                                                                                                                     
+}                                                                                                                                               
