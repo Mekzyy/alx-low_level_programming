6361dc5e450543469685915edb0c9c62fdeae5dd
Mekzyy <emeka.pesos@icloud.com>
2022-09-12 04:20:54 -0700

    done with task

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,20 @@
-#include <stdlib.h>                                                                                                                             
-#include <time.h>                                                                                                                               
-#include <stdio.h>                                                                                                                              
+#include <stdlib.h>                                                          
+#include <time.h>                                                                                
+#include <stdio.h>                      
 /**
- * main - lower case alphabets                                                                                                       
- *                                                                                                                                              
- * Description: using the main function                                                                                                         
+ * main - lower case alphabets               
+ *                                                                    
+ * Description: using the main function                                               
  * Return: 0                                                                                                                                    
- */                                                                                                                                               
-                                                                                                                                                
-int main(void)                                                                                                                                  
+ */                      
+                                               
+int main(void)                                 
 {     
 	char c;
 
 	for (c ='a'; c <='z'; c++)
 		putchar(c);
 
-	putchar('\n');	
-	return (0);                                                                                                                                     
-}                                                                                                                                               
+putchar('\n');
+return (0);                                                 
+}              
