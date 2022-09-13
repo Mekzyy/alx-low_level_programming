a28346248e65525e3ac90cff4c579af45e8ae6bf
Mekzyy <emeka.pesos@icloud.com>
2022-09-13 13:13:21 -0700

    done with task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - print alphabet
+ *
+ * Description - alphabets
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
