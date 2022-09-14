67edcb32d395c7e86b7918d1608c88504fc72e5f
Mekzyy <emeka.pesos@icloud.com>
2022-09-13 18:42:19 -0700

    done with task

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
 /**
  * print_last_digit - print the last digit of a number
  * @n: int type number
- * Return: return value of last digit 
+ * Return: return value of last digit
  */
 
 int print_last_digit(int n)
