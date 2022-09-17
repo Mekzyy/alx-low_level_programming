d527b538a8ccddbfda18e28b7b9ff45920bdbd08
Mekzyy <emeka.pesos@icloud.com>
2022-09-17 03:59:03 -0700

    done with task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - checks if uppercase
+ * @c: character to check
+ *
+ * Return: returns 1 if uppercase or 0 if otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A'&& c<= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
