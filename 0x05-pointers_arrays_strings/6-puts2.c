b6cd3767b00d1bb1f504e5afb4d717333729a805
Mekzyy <emeka.pesos@icloud.com>
2022-09-19 16:09:10 -0700

    done with task

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,19 +3,20 @@
 /**
  * puts2 - prints only one character
  * @str: input
- * Return: print
+ * Return: Void
  */
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	for (i; str[i] != '\0'; i++)
+	i = 0;
+
+	while (str[i] != '\0')
 	{
-		if ((i % 2) == 0)
+		if (i % 2 == 0)
 			_putchar(str[i]);
-		else
-			continue;
+		i++;
 	}
-	_putchar('\0');
+	_putchar('\n');
 }
