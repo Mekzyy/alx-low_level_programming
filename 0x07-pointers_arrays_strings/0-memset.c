d8fb48bfa849b688aa31e4aace963f49a4e420a0
Mekzyy <emeka.pesos@icloud.com>
2022-09-26 17:24:58 -0700

    done with task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,7 +6,7 @@
  * @s: pointer to memory area.
  * @b: constant to fill memory with.
  * @n: bytes of the memory area to be filled.
- * 
+ *
  * Return: pointer to memory area s.
  */
 char *_memset(char *s, char b, unsigned int n)
