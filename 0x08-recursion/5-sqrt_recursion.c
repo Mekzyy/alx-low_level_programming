ce5fde78c61716de8f14f098ea4c72bcb32c17df
Mekzyy <emeka.pesos@icloud.com>
2022-09-28 18:18:35 -0700

    done with task

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -19,10 +19,10 @@ int helper(int a, int b)
 	return (1);
 }
 /**
- *  * _sqrt_recursion - returns the natural square root of a number
- *   * @n: the number to return the square root of
- *    * Return: the square root of @n
- *     */
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to return the square root of
+ * Return: the square root of @n
+ */
 int _sqrt_recursion(int n)
 {
 	if (n == 0)
