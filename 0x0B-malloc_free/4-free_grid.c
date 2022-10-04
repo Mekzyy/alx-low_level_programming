00a8989b48f5dbd6cc6ccbffc42efa7294703961
Mekzyy <emeka.pesos@icloud.com>
2022-10-04 04:40:15 -0700

    done with malloc

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * free_grid - allocates a grid, make space and free space
- * grid: takes in width of grid
+ * @grid: takes in width of grid
  * @height: height of grid
  * Return: free grid
  */
