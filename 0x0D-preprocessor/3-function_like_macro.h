3598cf497d8bb34fbe5a2727c8b6684a9f2215d1
Mekzyy <emeka.pesos@icloud.com>
2022-10-10 19:45:41 -0700

    preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,6 @@
-#ifndef ABS_X
-#define ABS_X
-#define ABS(X) ((X < 0) ? (X * -1) : (X))
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
+
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
+
 #endif
