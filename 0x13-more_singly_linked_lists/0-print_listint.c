19fc636b0ef68974ceeaa6d8a12a5e4e7b5fe564
Mekzyy <emeka.pesos@icloud.com>
2022-10-25 13:22:53 -0700

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,19 +1,22 @@
 #include "lists.h"
-/*
- * print_listint - prints all the elements of a linked list
- * @h: pointer to the 1st node of the linked list
- * Return: number of nodes
+#include <stdio.h>
+
+/**
+ * print_listint -  prints all the elements of a listint_t list.
+ * @h: list to print
+ *
+ * Return: number of elements in the list
  */
+
 size_t print_listint(const listint_t *h)
 {
-	size_t nodenum = 0;
+	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		nodenum++;
 		h = h->next;
+		count++;
 	}
-
-	return (nodenum);
+	return (count);
 }
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,17 +1,20 @@
 #include "lists.h"
-/*
- * listint_len - returns the number of elements in a linked lists
- * @h: pointer to the 1st node of the linked list
- * Return: number of nodes
+
+/**
+ * listint_len - returns the number of elements in a linked listint_t list.
+ * @h: pointer to the linked list
+ *
+ * Return: number of elements in a linked listint_t list
  */
+
 size_t listint_len(const listint_t *h)
 {
-	size_t nodenum = 0;
+	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		nodenum++;
+		count++;
 		h = h->next;
 	}
-	return (nodenum);
+	return (count);
 }
