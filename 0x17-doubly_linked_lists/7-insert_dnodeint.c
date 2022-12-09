46a25f89b15447520efff7c4c3c919a811ea12e2
Mekzyy <emeka.pesos@icloud.com>
2022-12-08 20:56:22 -0800

    new

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,73 +1,45 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns the number of elements in a dlistint_t list
- * @h: head of doubly linked list
+ * insert_dnodeint_at_index - inserts a node at a given index
+ * in a doubly linked list
+ * @h: double pointer to the list
+ * @idx: index of the node to insert
+ * @n: data to insert
  *
- * Return: number of nodes
+ * Return: address of the new node, or NULL if it failed
  */
-
-size_t dlistint_len(const dlistint_t *h)
-{
-	int count = 0;
-
-	while (h)
-	{
-		count++;
-		h = h->next;
-	}
-	return (count);
-}
-
-/**
- * insert_dnodeint_at_index - inserts a new node at a given position
- * @h: head of linked list
- * @idx: index
- * @n: integer value of node
- *
- * Return: address of new node, return NULL if fails
- */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *temp;
-	size_t length;
-	unsigned int i = 0;
+	unsigned int i;
+	dlistint_t *new;
+	dlistint_t *temp = *h;
 
-	if (h == NULL)
+	new = malloc(sizeof(dlistint_t));
+	if (!new || !h)
 		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	length = dlistint_len(*h);
-	if (idx == length - 1)
-		return (add_dnodeint_end(h, n));
-
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	if (*h == NULL)
+	for (i = 0; temp && i < idx; i++)
 	{
-		new->prev = NULL;
-		new->next = NULL;
-		*h = new;
-		return (new);
-	}
-	temp = *h;
-	while (temp)
-	{
-		if (i == idx)
+		if (i == idx - 1)
 		{
-			new->next = temp;
-			new->prev = temp->prev;
-			temp->prev->next = new;
-			temp->prev = new;
+			if (temp->next == NULL)
+				return (add_dnodeint_end(h, n));
+			new->next = temp->next;
+			new->prev = temp;
+			temp->next->prev = new;
+			temp->next = new;
 			return (new);
 		}
-		temp = temp->next;
-		i++;
+		else
+			temp = temp->next;
 	}
-	free(new);
+
 	return (NULL);
 }
